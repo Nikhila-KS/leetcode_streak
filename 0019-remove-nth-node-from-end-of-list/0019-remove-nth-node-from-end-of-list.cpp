15fd730d018b83930f62f66dcff73a992948804b
Nikhila-KS <nikhila122btcse21@igdtuw.ac.in>
2022-11-21 19:33:10 +0530

    Time: 11 ms (33.52%), Space: 10.7 MB (32.20%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -10,38 +10,51 @@
  */
 class Solution {
 public:
-    ListNode* reversell(ListNode*head){
-        ListNode* prev=NULL;
-        ListNode* curr=head;
-        ListNode* frwd=head;
-        while(curr!=NULL){
-            frwd=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=frwd;
-        }
-        return prev;
-    }
+    // ListNode* reversell(ListNode*head){
+    //     ListNode* prev=NULL;
+    //     ListNode* curr=head;
+    //     ListNode* frwd=head;
+    //     while(curr!=NULL){
+    //         frwd=curr->next;
+    //         curr->next=prev;
+    //         prev=curr;
+    //         curr=frwd;
+    //     }
+    //     return prev;
+    // }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head==NULL){
-            return NULL;
-        }
-        if(head->next==NULL && n>=1){
-            return NULL;
-        }
-        ListNode* nhead=reversell(head);
-        ListNode* temp=nhead;
-        if(n==1){
-            nhead=nhead->next;
-            nhead=reversell(nhead);
-            return nhead;
-        }
-        for(int i=1;i<n-1;i++){
-            temp=temp->next;
-        }
-        temp->next=temp->next->next;
-        nhead=reversell(nhead);
-        return nhead;
+        // if(head==NULL){
+        //     return NULL;
+        // }
+        // if(head->next==NULL && n>=1){
+        //     return NULL;
+        // }
+        // ListNode* nhead=reversell(head);
+        // ListNode* temp=nhead;
+        // if(n==1){
+        //     nhead=nhead->next;
+        //     nhead=reversell(nhead);
+        //     return nhead;
+        // }
+        // for(int i=1;i<n-1;i++){
+        //     temp=temp->next;
+        // }
+        // temp->next=temp->next->next;
+        // nhead=reversell(nhead);
+        // return nhead;
+        
+        ListNode* iter = head;
+        int len = 0, i = 1;
+        while(iter) iter = iter -> next, len++;    // finding the length of linked list
+        if(len == n) return head -> next;          // if head itself is to be deleted, just
+        
+        for(iter = head; i < len - n; i++) iter = iter -> next; // iterate first len-n nodes
+        iter -> next = iter -> next -> next;      // remove the nth node from the end
+        return head;
         
     }
 };
+
+
+
+
