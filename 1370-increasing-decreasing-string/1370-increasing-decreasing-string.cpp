ba2e1bcb5564f0556a6b54a09e93e943e44cf56f
Nikhila-KS <nikhila122btcse21@igdtuw.ac.in>
2022-11-22 20:07:57 +0530

    Time: 11 ms (78.27%), Space: 7.4 MB (80.80%) - LeetHub

--- 1370-increasing-decreasing-string/1370-increasing-decreasing-string.cpp
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.cpp
@@ -28,21 +28,3 @@ public:
     }
 };
 
-//        string res="";
-//         int a[26]={0},k=0;
-//         for(int i=0;i<s.length();i++)
-//         {a[s[i]-'a']++;k++;}
-//         while(k)
-//         {
-//             for(int i=0;i<26;i++)
-//             {
-//                 if(a[i]>0) { res+=i+'a';k--;a[i]--;}
-//             }
-//             for(int i=25;i>=0;i--)
-//             {
-//                 if(a[i]>0) { res+=i+'a';k--;a[i]--;}
-//             }
-//         }
-        
-        
-//         return res;
