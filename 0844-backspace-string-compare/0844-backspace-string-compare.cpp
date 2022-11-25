478ec1da8bd0bc2dfdb84e7d6186763e228323ea
Nikhila-KS <nikhila122btcse21@igdtuw.ac.in>
2022-11-25 19:38:42 +0530

    Time: 4 ms (46.78%), Space: 6.3 MB (71.42%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,38 +1,85 @@
+// class Solution {
+// public:
+//     bool backspaceCompare(string s, string t) {
+//         vector<char> ss;
+//         vector<char> tt;
+//         for(int i=0;i<s.size();i++){
+//             if(ss.size()==0 && s[i]=='#'){
+//                 continue;
+//             }
+//             if(s[i]=='#'){
+//                 ss.pop_back();
+//                 continue;
+//             }                                                  //approach 1- my approach
+//             ss.push_back(s[i]);
+//         }
+//         for(int i=0;i<t.size();i++){
+//             if(tt.size()==0 && t[i]=='#'){
+//                 continue;
+//             }
+//             if(t[i]=='#'){
+//                 tt.pop_back();
+//                 continue;
+//             }
+//             tt.push_back(t[i]);
+//         }
+//         while(tt.size()!=0 && ss.size()!=0){
+//             if(tt.back()!=ss.back()){
+//                 return false;
+//             }
+//             tt.pop_back();
+//             ss.pop_back();
+//         }
+//         if(tt.size()!=0 || ss.size()!=0){
+//             return false;
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        vector<char> ss;
-        vector<char> tt;
-        for(int i=0;i<s.size();i++){
-            if(ss.size()==0 && s[i]=='#'){
-                continue;
+        int k=0,p=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='#')
+            {
+                k--;
+                 k=max(0,k);
             }
-            if(s[i]=='#'){
-                ss.pop_back();
-                continue;
-            }
-            ss.push_back(s[i]);
+            
+           else
+           {
+               s[k]=s[i];
+               k++;
+           }
         }
-        for(int i=0;i<t.size();i++){
-            if(tt.size()==0 && t[i]=='#'){
-                continue;
+        for(int i=0;i<t.size();i++)
+        {
+            if(t[i]=='#')
+            {
+                p--;
+                 p=max(0,p);
             }
-            if(t[i]=='#'){
-                tt.pop_back();
-                continue;
-            }
-            tt.push_back(t[i]);
+            
+           else
+           {
+               t[p]=t[i];
+               p++;
+           }
         }
-        while(tt.size()!=0 && ss.size()!=0){
-            if(tt.back()!=ss.back()){
-                return false;
-            }
-            tt.pop_back();
-            ss.pop_back();
-        }
-        if(tt.size()!=0 || ss.size()!=0){
+        if(k!=p)
             return false;
+        else
+        {
+            for(int i=0;i<k;i++)
+            {
+                if(s[i]!=t[i])
+                    return false;
+            }
+            return true;
         }
-        return true;
+        
     }
 };
