f3e374b1b2403b32cbdbd9e785425d8cbd6bc9ab
Nikhila-KS <nikhila122btcse21@igdtuw.ac.in>
2022-12-05 19:15:37 +0530

    Time: 16 ms (85.43%), Space: 21.4 MB (76.41%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -11,20 +11,31 @@
  */
 class Solution {
 public:
+    
     TreeNode* sortedArrayToBSThelper(vector<int>& nums, int si, int ei) {
 
         if(si>ei){
             return NULL;          // ei=ending index , si=starting index
         }
         
-        int mid=(si+ei)/2;
+        //find middle element from vector nums call recusion to get left and right child
+        
+        int mid= si+(ei-si)/2;
+        
         TreeNode*root=new TreeNode(nums[mid]);
-        root->left=sortedArrayToBSThelper(nums,si,mid-1);
-        root->right=sortedArrayToBSThelper(nums,mid+1,ei);
+        
+        root->left = sortedArrayToBSThelper(nums,si,mid-1);
+        root->right = sortedArrayToBSThelper(nums,mid+1,ei);
+        
         return root;
     }
     
+    
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return sortedArrayToBSThelper(nums,0,nums.size()-1);
+        
+        return sortedArrayToBSThelper(nums,0,nums.size()-1);      
+    
     }
+    
+    
 };
