6cf7c807f4560e3b1eae1ba8ff73e8e0f0ecd730
Nikhila-KS <nikhila122btcse21@igdtuw.ac.in>
2022-12-05 18:39:46 +0530

    Time: 20 ms (72.31%), Space: 21 MB (55.40%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,19 +11,36 @@
  */
 class Solution {
 public:
-   int height(TreeNode *r)
-    {
-        if(r==NULL)
-        return 0;
-        int l=height(r->left);
-        int h=height(r->right);
-        return 1+max(l,h);
-    }
-    bool isBalanced(TreeNode* r) 
-    {
-        if(r==NULL)                                            // abs- absolute function
-        return true;
-       return (abs(height(r->left)-height(r->right))<=1 && isBalanced(r->left)&&isBalanced(r->right));
+   
+//     int height(TreeNode *r)
+//     {
+//         if(r==NULL)
+//         return 0;
+//         int l=height(r->left);
+//         int h=height(r->right);
+//         return 1+max(l,h);
+//     }
+//     bool isBalanced(TreeNode* r) 
+//     {
+//         if(r==NULL)                                            // abs- absolute function
+//         return true;
+//        return (abs(height(r->left)-height(r->right))<=1 && isBalanced(r->left)&&isBalanced(r->right));
         
+//     }
+    
+    
+    bool flag=true;
+    int postorder(TreeNode* root){
+        if(root==NULL) return 0;
+        int l=postorder(root->left);
+        int r=postorder(root->right);
+        
+        if(abs(l-r)>1) flag=false;    //height of left subtre - height of right subtree
+        
+        return max(l,r)+1;  //returning height of the subtree
+    }
+    bool isBalanced(TreeNode* root) {
+        postorder(root);
+        return flag;
     }
 };
