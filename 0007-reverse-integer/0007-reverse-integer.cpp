d332ce9143f89d36a54492f13a948f3685f0b59a
Nikhila-KS <nikhila122btcse21@igdtuw.ac.in>
2022-12-12 14:16:47 +0530

    Time: 3 ms (69.37%), Space: 6 MB (32.88%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long y=0;
+        while(x!=0){
+            y=(y*10)+x%10;
+            if(y>INT_MAX|| y<INT_MIN)
+                return 0;
+            x=x/10;
+        }
+        return y;
+    }
+};
