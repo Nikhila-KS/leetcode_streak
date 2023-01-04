29c1745b5f25c299a9841f97b50adea56e8eca9e
Nikhila-KS <nikhilapersonal122@gmail.com>
2023-01-04 19:39:06 +0530

    Time: 359 ms (53.62%), Space: 103.5 MB (89.36%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,47 +1,7 @@
 class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
-        // vector<int> difflevel(10,0);
-        // for(int i=0;i<tasks.size();i++){
-        //     difflevel[tasks[i]]+=1;
-        // }
-        // int count=0;
-        // bool isbefore=false;
-        // int isbeforenum=0;
-        // for(int i=0;i<difflevel.size();i++){
-        //     if(difflevel[i]%3==0 && difflevel[i]>0){
-        //         count=count+(difflevel[i]/3);
-        //         difflevel[i]=0;
-        //     }else if(difflevel[i]/3 >0 && difflevel[i]>0){
-        //         count=count+floor(difflevel[i]/3);
-        //         difflevel[i]=difflevel[i]%3;
-        //         if(isbefore){
-        //             count++;
-        //             difflevel[i]=difflevel[i]-1;
-        //             isbeforenum--;
-        //             isbefore=(difflevel[i] || isbeforenum);
-        //         }
-        //     }else{
-        //         if(isbefore){
-        //             count++;
-        //             difflevel[i]=difflevel[i]-1;
-        //             isbeforenum--;
-        //             isbefore=(difflevel[i] || isbeforenum);
-        //             if((difflevel[i]!=0)&&(isbefore==true)){
-        //                 i--;
-        //             }
-        //         }else{
-        //             isbefore=true;
-        //             isbeforenum=difflevel[i];
-        //         }
-        //     }
-        // }
-        // if(isbefore==false){
-        //     return count;
-        // }else{
-        //     return -1;
-        // }
-        
+        // understand the question !!!!!!
           unordered_map<int,int> mp;
         for(int i=0;i<tasks.size();i++){
             mp[tasks[i]]++;
